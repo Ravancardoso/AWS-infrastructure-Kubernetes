cbe8e013c0238db65e4159c36a3f8d9a61de24b1
Ravan cardoso <ravanreis@gmail.com>
2025-10-23 15:42:04 -0300

    new feature eks-projects

---
+++ EKS-projects/providers.tf
@@ -0,0 +1,15 @@
+# provider
+provider "aws" {
+  region = "us-east-1" 
+}
+
+
+# terraform state
+
+#terraform {
+ # backend "s3" {
+    #bucket = "terraform-state-project-eks-aws"
+   # key    = "path/to/my/key"
+   # region = "us-east-1"
+ # }
+#}
